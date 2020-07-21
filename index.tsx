e5e52ff1eac7218567440f75e40a5cd11498ff7a
risingbirdsong <heregoingthere@outlook.com>
2020-07-20 20:59:39 -0700

    neat circle made of animating rectangles

--- index.tsx
+++ index.tsx
@@ -7,26 +7,24 @@ interface shipI {
 }
 
 const App = new p5((s: p5) => {
+  const rotatedArray: number[][] = [];
   let ship: shipI = {
     x: 100,
     y: 100,
-    angle: 0,
+    angle: 1,
   };
   s.setup = () => {
     s.createCanvas(800, 800);
     s.background(100);
   };
-  s.keyPressed = function () {
-    if ((s.keyCode = s.UP_ARROW)) {
-      ship.angle++;
-    }
-    console.log(s.keyCode);
-  };
+
   s.draw = () => {
     s.fill(10, 50, 230);
     s.translate(s.width / 2, s.height / 2);
     s.rotate(ship.angle);
     s.rect(ship.x, ship.y, 100, 20);
+    rotatedArray.push([ship.x, ship.y]);
+    ship.angle++;
   };
 });
 
